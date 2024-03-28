8da645cb3fc7760ba5bd73fabf9730cc38634ebd
grgcharles <165218884+grgcharles@users.noreply.github.com>
2024-03-28 15:12:18 +0800

    change headers Dashboard to GRG CM Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
     <Box m="20px">
       {/* HEADER */}
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="GRG CM DASHBOARD" subtitle="Welcome to your dashboard" />
 
         <Box>
           <Button
